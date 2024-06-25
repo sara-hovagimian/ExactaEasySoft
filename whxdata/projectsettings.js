27565db6fb006c81a395a487ed8e7108c86d1565
Sara Hovagimian <sarahovamanuk@gmail.com>
2024-06-25 17:31:34 -0300

    actualizacion de anio fiscal 25-06-24

--- whxdata/projectsettings.js
+++ whxdata/projectsettings.js
@@ -7,7 +7,7 @@ rh._.exports(defaultTopic);
 rh.consts('DEFAULT_TOPIC', encodeURI("html/addDeleteModify.html"));
 rh.consts('HOME_FILEPATH', encodeURI('index.htm'));
 rh.consts('START_FILEPATH', encodeURI('index.htm'));
-rh.consts('HELP_ID', '7d8b5280-dcd2-4021-816f-20ce7401a0cd' || 'preview');
+rh.consts('HELP_ID', 'f3382c0c-245e-49e8-8536-fff15d9e3ca4' || 'preview');
 rh.consts('LNG_SUBSTR_SEARCH', 0);
 
 model.publish(rh.consts('KEY_LNG_NAME'), "es");
